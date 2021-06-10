029f983a84785fa08d32002aadba66eed1c0d4cf
DarkIceXD <15961071+DarkIceXD@users.noreply.github.com>
2021-06-10 17:15:45 +0200

    added component

---
+++ frontend/src/components/Message.vue
@@ -0,0 +1,31 @@
+<template>
+  <div v-if="right == true">
+    <div class="chat-r">
+      <div class="sp"></div>
+      <div class="mess mess-r">
+        <p>{{ msg }}</p>
+      </div>
+    </div>
+  </div>
+  <div v-else>
+    <div class="chat-l">
+      <div class="mess">
+        <p>{{ "aaaa" }}</p>
+      </div>
+      <div class="sp"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Message",
+  props: {
+    msg: String,
+    right: Boolean,
+  },
+};
+</script>
+
+<style>
+</style>
