84b4c445ad57da2c3d75a8ec8d42e276bff14d68
robinsadeghpour <38283032+robinsadeghpour@users.noreply.github.com>
2021-06-19 00:25:12 +0200

    GPT3 qna implemented

--- backend/api/api.py
+++ backend/api/api.py
@@ -7,8 +7,8 @@ from backend.api import machine_learning_qna_engine
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
-with open("config.json") as json_data_file:
-    config = json.load(json_data_file)
+with open("config.json") as json_config_file:
+    config = json.load(json_config_file)
 
 
 @app.route("/", methods=["GET"])
@@ -18,7 +18,9 @@ def home():
 
 @app.route("/tutorai/machine_learning/<question>/<context>", methods=["GET"])
 def machine_learning_question_answering_api(question, context):
-    return jsonify(answer=machine_learning_qna_engine.get_answer(str(question), str(context)))
+    answer = machine_learning_qna_engine.get_answer(str(question), str(context))
+    print("ANSWER "+ answer)
+    return jsonify(answer=answer)
 
 
 if __name__ == '__main__':
--- backend/api/machine_learning_qna_engine/__init__.py
+++ backend/api/machine_learning_qna_engine/__init__.py
@@ -1,2 +1,73 @@
+import json
+from transformers import pipeline
+import openai
+import os
+
+DEFAULT_ANSWER = "Sorry, I´m not sure"
+SCORE_TOLERATION = 0.05
+
+
+def get_config_file():
+    global config
+
+    path_to_config_file = os.path.join(os.path.dirname(__file__), "config.json")
+    relative_path_to_config_file = os.path.relpath(path_to_config_file)
+    with open(relative_path_to_config_file) as json_config_file:
+        config = json.load(json_config_file)
+        print("Halllooooo:  ")
+        print(config)
+
+
+get_config_file()
+
+
 def get_answer(question: str, context: str) -> str:
-    return question+context
+
+    if config["ml_config"]["model"] == "GPT3":
+        response = gpt3_question_answerer(question, context)
+    else:
+        response = bert_question_answerer(question, context)
+
+    print("RESPONSE: " + response)
+    return response
+
+
+def bert_question_answerer(question, context):
+    question_answerer = pipeline('question-answering')
+
+    payload = {
+        'question': question,
+        'context': context
+    }
+    response = question_answerer(payload)
+
+    if response["score"] < config["ml_config"]["score_toleration"]:
+        return DEFAULT_ANSWER
+    else:
+        return response["answer"]
+
+
+def gpt3_question_answerer(question, context):
+    openai.api_key = config["GPT3_config"]["api_key"]
+
+    question_sequence = "\n\nQ: " + question
+    answere_sequence = "\nA: "
+
+    print(context + question_sequence + answere_sequence)
+    response = openai.Completion.create(
+        engine="davinci",
+        prompt=context + question_sequence + answere_sequence,
+        max_tokens=config["GPT3_config"]["max_tokens"],
+        temperature=0,
+        top_p=1,
+        frequency_penalty=0.0,
+        presence_penalty=0.0,
+        stop=["\n"]
+    )
+    print(response["choices"][0]["text"])
+
+    return response["choices"][0]["text"]
+
+
+if __name__ == '__main__':
+    get_config_file()
